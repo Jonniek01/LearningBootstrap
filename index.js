b828c12b17dff21770c6376a97962e2644caa948
Jonniek01 <jontejonhn@gmail.com>
2021-10-20 19:20:42 +0300

    second commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//toggling play and pause buttons interchangebly
+/*function interchange(){
+   var pause=document.getElementById("carousel-pause");
+   var play=document.getElementById("carousel-play");
+   play.style.display="none";
+   pause.onclick=function(){
+       play.style.display="block";
+       pause.style.display="none";
+   }
+   play.onclick=function(){
+    play.style.display="none";
+    pause.style.display="block";
+}
+   }
+   interchange();
+*/
+
